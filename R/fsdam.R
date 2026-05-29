fa8d0c5f5be38ca1e0ba9fe77e716ff3d4f91229
Sunwoo Han <64387506+shan-stat@users.noreply.github.com>
2026-05-29 10:29:42 -0400

    Update fsdam.R

--- R/fsdam.R
+++ R/fsdam.R
@@ -1,5 +1,6 @@
 #library(reticulate); library(FSDAM); dat=hvtn505tier1[,-1]; opt_numCode=2; opt_seed=1; opt_model="n"; opt_gpu=0; opt_k=100; opt_nEpochs=500; opt_constr="newpenalization"; opt_tuneParam=10; opt_penfun="mean"; opt_ortho=1; opt_earlystop="no"
-fsdam <- function(dat, opt_numCode=ncol(dat), opt_seed=1, opt_model="n", opt_gpu=0, opt_k=100, opt_nEpochs=10000, opt_constr=c("newpenalization", "constrained", "none"), opt_tuneParam=10, opt_penfun="mean", opt_ortho=1, opt_earlystop="no", verbose=FALSE) {    
+# SH : opt_klParam added
+fsdam <- function(dat, opt_numCode=ncol(dat), opt_seed=1, opt_model="n", opt_gpu=0, opt_k=100, opt_nEpochs=10000, opt_constr=c("newpenalization", "constrained", "none"), opt_tuneParam=10, opt_klParam=1, opt_penfun="mean", opt_ortho=1, opt_earlystop="no", verbose=FALSE) {    
 
     if (!reticulate::py_module_available("torch")) stop("Python torch module is not available. Try restarting R, use a different Python installation by reticulate::use_python(path) before calling fsdam") 
 #    reticulate::use_python("/app/easybuild/software/Python/3.7.4-foss-2016b/bin/python")
@@ -9,13 +10,15 @@ fsdam <- function(dat, opt_numCode=ncol(dat), opt_seed=1, opt_model="n", opt_gpu
     ae <- import_from_path("autoencoders", path = python_path)  
     
     if (!is.data.frame(dat)) dat=data.frame(dat)    
-    
-    out=ae$fsdam$main(dat, as.integer(opt_numCode), as.integer(opt_seed), opt_model, as.integer(opt_gpu), as.integer(opt_k), as.integer(opt_nEpochs), opt_constr, opt_tuneParam, opt_penfun, opt_ortho, opt_earlystop, verbose)    
-    for (i in 1:length(out)) names(out[[i]])=c("reconstruct_loss", "pen_loss", "y_pred", "code", "decoder_w", "decoder_b", "history")
+
+    # SH : opt_klParam added; "kl_loss" added
+    out=ae$fsdam$main(dat, as.integer(opt_numCode), as.integer(opt_seed), opt_model, as.integer(opt_gpu), as.integer(opt_k), as.integer(opt_nEpochs), opt_constr, opt_tuneParam, opt_klParam, opt_penfun, opt_ortho, opt_earlystop, verbose)    
+    for (i in 1:length(out)) names(out[[i]])=c("reconstruct_loss", "kl_loss", "pen_loss", "y_pred", "code", "decoder_w", "decoder_b", "history")
     
     ret = list(
           data=dat
         , mse=sapply(1:opt_numCode, function (i) out[[i]]$reconstruct_loss)
+        , kl.loss=sapply(1:opt_numCode, function (i) out[[i]]$kl_loss)
         , pen.loss=sapply(1:opt_numCode, function (i) out[[i]]$pen_loss)
         , code=sapply(1:opt_numCode, function (i) out[[i]]$code$cpu()$numpy())
         , prediction=lapply(1:opt_numCode, function (i) out[[i]]$y_pred$cpu()$numpy()) # cpu() is needed if gpu is used
